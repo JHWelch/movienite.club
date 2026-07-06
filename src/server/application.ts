ad4a5503a7d6c7085a1b65f4665051a1b37b09e6
Jordan Welch <jordanhwelch@gmail.com>
2026-07-05 20:44:40 -0500

    Move newing up objects into Application

--- src/server/application.ts
+++ src/server/application.ts
@@ -1,6 +1,6 @@
 import { type Express } from 'express'
 import ViteExpress from 'vite-express'
-import type NotionAdapter from '@server/data/notion/notionAdapter'
+import NotionAdapter from '@server/data/notion/notionAdapter'
 import FirestoreAdapter from '@server/data/firestore/firestoreAdapter'
 import TmdbAdapter from '@server/data/tmdb/tmdbAdapter'
 import createAppRouter from '@server/routers/appRouter'
@@ -13,14 +13,16 @@ export default class Application {
   constructor (
     private config: Config,
     private express: Express,
-    discord: DiscordAdapter,
-    firestore: FirestoreAdapter,
-    notion: NotionAdapter,
-    tmdb: TmdbAdapter,
   ) {
     express.use('/cron', cronMiddleware)
     express.use('/api/cache', passwordMiddleware(config))
-    express.use(createAppRouter(config, discord, firestore, notion, tmdb))
+    express.use(createAppRouter(
+      config,
+      new DiscordAdapter(config),
+      new FirestoreAdapter(config),
+      new NotionAdapter(config),
+      new TmdbAdapter(config),
+    ))
   }
 
   listen (): void {
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,21 +1,11 @@
 import * as dotenv from 'dotenv'
 import Application from '@server/application'
-import NotionAdapter from '@server/data/notion/notionAdapter'
-import FirestoreAdapter from '@server/data/firestore/firestoreAdapter'
 import setupExpress from '@server/config/express'
-import TmdbAdapter from '@server/data/tmdb/tmdbAdapter'
 import Config from '@server/config/config'
-import DiscordAdapter from '@server/data/discord/discordAdapter'
 
 dotenv.config()
 
-const config = new Config()
-
 new Application(
-  config,
+  new Config(),
   setupExpress(),
-  new DiscordAdapter(config),
-  new FirestoreAdapter(config),
-  new NotionAdapter(config),
-  new TmdbAdapter(config),
 ).listen()
